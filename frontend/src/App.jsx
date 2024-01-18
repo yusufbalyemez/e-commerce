ae118a96da6f3306c2a8923d8ab7eaab3e767122
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-01-18 23:30:15 +0300

    Sliders Component Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Layout/Header/Header'
 import Footer from './components/Layout/Footer/Footer'
 import Policy from './components/Layout/Policy/Policy'
+import Slider from './components/Layout/Slider/Sliders'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Slider/>
       <Policy/>
       <Footer/>
     </div>
