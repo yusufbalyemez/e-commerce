9d18ad0d7ff6b97f2fd37af48c5832767d83ece5
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-01-18 14:45:33 +0300

    İlk kurulum

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
