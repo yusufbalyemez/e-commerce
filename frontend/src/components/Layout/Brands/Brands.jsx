20cc554ba03d25c781f1e6febd8fd16275b04f35
Yusuf Balyemez <124602727+yusufbalyemez@users.noreply.github.com>
2024-01-20 14:44:29 +0300

    Brands Component added

---
+++ frontend/src/components/Layout/Brands/Brands.jsx
@@ -0,0 +1,43 @@
+import "./Brands.css"
+const Brands = () => {
+    return (
+        <section className="brands">
+    <div className="container">
+      <ul className="brand-list">
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand1.png" alt="" />
+          </a>
+        </li>
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand2.png" alt="" />
+          </a>
+        </li>
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand3.png" alt="" />
+          </a>
+        </li>
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand4.png" alt="" />
+          </a>
+        </li>
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand5.png" alt="" />
+          </a>
+        </li>
+        <li className="brand-item">
+          <a href="#">
+            <img src="img/brands/brand1.png" alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+    )
+}
+
+export default Brands
