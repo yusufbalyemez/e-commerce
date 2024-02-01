fed15e245ab23c5fe6a74cce8008bd4c0bcffa2b
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-02-01 13:29:36 +0300

    Bir input içerisindeki değer toplu olarak frontendde nasıl alınır

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react"
+
 const Register = () => {
+
+    //toplu useState yöntemi
+    //inputtaki name kısmıda burada verdiklerimle aynı olmalı ya da buradakiler oradakilerle aynı olmalı
+    const [formData,setFormData]=useState({
+        username:"",
+        email:"",
+        password:""
+    });
+
+    //inputtaki verileri toplu almak için
+    const handleInputChanged = (e) =>{
+        const {name,value} = e.target;
+        setFormData({...formData, [name]:value}) //tümünü yerleştir, name bilgisine göre value yi al demekmiş
+    }
+
+    console.log(formData);
+
     return (
         <div className="account-column">
                     <h2>Register</h2>
@@ -6,19 +25,19 @@ const Register = () => {
                         <div>
                             <label>
                                 <span>Username <span className="required">*</span></span>
-                                <input type="text" />
+                                <input type="text" name="username" onChange={handleInputChanged} />
                             </label>
                         </div>
                         <div>
                             <label>
                                 <span>Email address <span className="required">*</span></span>
-                                <input type="email" />
+                                <input type="email" name="email" onChange={handleInputChanged} />
                             </label>
                         </div>
                         <div>
                             <label>
                                 <span>Password <span className="required">*</span></span>
-                                <input type="password" />
+                                <input type="password" name="password" onChange={handleInputChanged} />
                             </label>
                         </div>
                         <div className="privacy-policy-text remember">
